95a3464471b8f14cb0263dba1128667fc23d1a9c
Vivecboots <Blairwin05@gmail.com>
2023-08-12 16:14:08 -0700

    added globe! turn into an inception FF8 video game?

--- src/lib/DancingFigure.svelte
+++ src/lib/DancingFigure.svelte
@@ -5,79 +5,115 @@
     import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
     let figureContainer;
+    let mixer;
+    let defaultAction, backflipAction;
 
     onMount(() => {
-        const scene = new THREE.Scene();
+        const scene = setupScene();
+        const camera = setupCamera();
+        const renderer = setupRenderer();
+        const controls = setupControls(camera, renderer);
+        setBackground(renderer);
+        loadModels(scene);
 
-        // Lights
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-        scene.add(ambientLight);
+        figureContainer.addEventListener('click', handleFigureClick);
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(0, 1, 1).normalize();
-        scene.add(directionalLight);
+        animate(scene, camera, renderer, controls);
+    });
 
+    function setupScene() {
+    const scene = new THREE.Scene();
+
+    // Lights
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0, 1, 1).normalize();
+    scene.add(directionalLight);
+
+    // Skybox
+    const textureLoader = new THREE.TextureLoader();
+    const bgTexture = textureLoader.load('/st_peters_square_night_4k.png');
+    const skyboxGeometry = new THREE.SphereGeometry(500, 60, 40);
+    const skyboxMaterial = new THREE.MeshBasicMaterial({ map: bgTexture, side: THREE.BackSide });
+    const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+    scene.add(skybox);
+
+    return scene;
+}
+
+
+    function setupCamera() {
         const camera = new THREE.PerspectiveCamera(75, figureContainer.clientWidth / figureContainer.clientHeight, 0.1, 1000);
         camera.position.z = 5;
+        return camera;
+    }
 
+    function setupRenderer() {
         const renderer = new THREE.WebGLRenderer({ alpha: true });
         renderer.setClearColor(0x000000, 0);
         renderer.setSize(figureContainer.clientWidth, figureContainer.clientHeight);
         figureContainer.appendChild(renderer.domElement);
+        return renderer;
+    }
 
+    function setupControls(camera, renderer) {
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.minDistance = camera.position.z / 3;
         controls.maxDistance = camera.position.z * 5;
         controls.zoomSpeed = 0.5;
+        return controls;
+    }
 
-        let mixer;
-        let defaultAction, backflipAction;
+    function setBackground(renderer) {
+        const textureLoader = new THREE.TextureLoader();
+        const bgTexture = textureLoader.load('/city-7091684_1280.jpg');
+        renderer.background = bgTexture;
+    }
 
+    function loadModels(scene) {
         const loader = new GLTFLoader();
-        loader.load(
-            '/checkwatch.glb',
-            (gltf) => {
-                gltf.scene.scale.set(0.88, 0.88, 0.88);
-                scene.add(gltf.scene);
+        loader.load('/checkwatch.glb', (gltf) => {
+            gltf.scene.scale.set(0.88, 0.88, 0.88);
+            scene.add(gltf.scene);
 
-                if (gltf.animations && gltf.animations.length) {
-                    mixer = new THREE.AnimationMixer(gltf.scene);
-                    defaultAction = mixer.clipAction(gltf.animations[0]);
-                    defaultAction.play();
+            if (gltf.animations && gltf.animations.length) {
+                mixer = new THREE.AnimationMixer(gltf.scene);
+                defaultAction = mixer.clipAction(gltf.animations[0]);
+                defaultAction.play();
 
-                    loader.load('/backflip.glb', (backflipGltf) => {
-                        if (backflipGltf.animations && backflipGltf.animations.length) {
-                            backflipAction = mixer.clipAction(backflipGltf.animations[0]);
-                        }
-                    });
-                }
-            }
-        );
-
-        figureContainer.addEventListener('click', () => {
-            if (mixer && backflipAction) {
-                mixer.stopAllAction();
-                backflipAction.reset().setLoop(THREE.LoopOnce).play();
-
-                setTimeout(() => {
-                    defaultAction.reset().play();
-                }, 1300);
+                loader.load('/backflip.glb', (backflipGltf) => {
+                    if (backflipGltf.animations && backflipGltf.animations.length) {
+                        backflipAction = mixer.clipAction(backflipGltf.animations[0]);
+                    }
+                });
             }
         });
+    }
 
-        const animate = () => {
-            requestAnimationFrame(animate);
+    function handleFigureClick() {
+        if (mixer && backflipAction) {
+            mixer.stopAllAction();
+            backflipAction.reset().setLoop(THREE.LoopOnce).play();
 
+            setTimeout(() => {
+                defaultAction.reset().play();
+            }, 1300);
+        }
+    }
+
+    function animate(scene, camera, renderer, controls) {
+        const render = () => {
+            requestAnimationFrame(render);
             if (mixer) {
                 mixer.update(0.01);
             }
-
             controls.update();
             renderer.render(scene, camera);
         };
-
-        animate();
-    });
+        render();
+    }
 </script>
 
 <div bind:this={figureContainer} class="figure-container col-span-2">
@@ -95,9 +131,3 @@
         height: 100%;
     }
 </style>
-
-
-
-
-
-
