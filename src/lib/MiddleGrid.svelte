7fe3ae201d54405d99dc0d24e8f86b6b8fb848e6
Vivecboots <Blairwin05@gmail.com>
2023-08-15 11:06:53 -0700

    add intro words

--- src/lib/MiddleGrid.svelte
+++ src/lib/MiddleGrid.svelte
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap">
+
+
 <style>
     @keyframes glowing {
         0%, 100% {
@@ -9,10 +12,13 @@
     }
 
     .grid-container {
+        padding-left: 2%;
+        padding-right: 2%;
         display: grid;
-        grid-template-columns: repeat(6, 1fr);
+        grid-template-columns: repeat(8, 1fr);
         gap: 16px;
-        width: 88vw;
+        width: 96vw;
+        height: 23vh;
         margin-top: 6%;
         margin-bottom: 10%;
     }
@@ -24,11 +30,11 @@
     }
 
     .narrative-first {
-        grid-column: 1/4;
+        grid-column: 1/3;
     }
 
     .narrative-second {
-        grid-column: 4/7;
+        grid-column: 6/9;
     }
 
     .skeleton-card-first, .skeleton-card-second {
@@ -37,17 +43,15 @@
         border-radius: 8px;
         color: #010101;
         animation: glowing 10s infinite; /* Apply the glowing effect */
-        font-family: 'PressStart2P', sans-serif;
-        font-size: 1.5rem;
-        font-weight: bold;
+        font-family: 'Press Start 2P', cursive;
     }
 
     .skeleton-card-first {
-        grid-column: 4/7;
+        grid-column: 3/6;
     }
 
     .skeleton-card-second {
-        grid-column: 1/4;
+        grid-column: 1/5;
     }
 
     @media (max-width: 768px) {
@@ -58,20 +62,35 @@
 
         .narrative-first, .skeleton-card-first, .narrative-second, .skeleton-card-second {
             grid-column: 1 / -1;
+            margin-right: 0;
+            margin-left: 0;
         }
     }
 </style>
+
+
+
+
 
 <div class="grid-container">
     <!-- First row: picture then card -->
     <div class="narrative-first"></div>
     <div class="skeleton-card-first">
-        While crypto is a great conduit of money, at over 1 trillion USD and expected to be multiples of that in the near future, the true value is actually in identity and state change management. Blockchain technology allows permanence in a technological realm where cloning identities takes no more work than clicking a few buttons. As work becomes cheaper and easier, problems of verification will only become more difficult. To ensure ownership in a technological future is challenging, but it is also critical. That is why despite recent downturns, some technologies that are encompassed in the ideology of Web3 are a glimpse into how we will operate in the future. When I was a kid, they said home computers would never progress past toys. Many other predictions came out in my life like that, and many of them were incorrect. I recommend embracing the technology early and piggyback on the great things it offers. Places of extreme interest for me, and what embodies my ideologies, are that of ownership, privacy, and equality. Equality of opportunity, fees, interoperability, and zero-knowledge proofs are among some of the many ways we can achieve this. I see working in this field as an opportunity to make a difference in something that will affect humans for the rest of time as our economy becomes digital. When working in emerging fields, each action taken is amplified. As of now, I’m writing in Typescript, a subset of Javascript, and the reductions keep going until you are left with a person who made a small decision one day to name a function if. And that if changed everything. And that's what led me here, one question about what I stand for as a person, if, if this is what I believe in, then what are the consequences if I don’t do it. I hope to work with only teams that feel the same passion. Finances attract scammers, certainly. But the stock financial sector doesn't let it define it. Rather, they are prestigious. And one day, as the younger generations grow into the role, so will the new technology.
+        In the digital frontier, the valuation of cryptocurrencies, now eclipsing 1 trillion USD, is merely a chapter in a broader story. Blockchain technology's strength lies in its unparalleled ability to safeguard identity and manage intricate state changes in a world where digital replication is all too easy. As we advance technology, the need for robust verification intensifies.
+
+Web3, with its market dynamics, is undeniably the blueprint for our digital future. Past underestimations, like those concerning home computers, emphasize the significance of early immersion in transformative technologies.
+
+At the heart of my Web3 commitment are the principles of ownership, privacy, and equality. I champion interoperability that's both seamless and cost-effective. A fair fee structure, irrespective of scale or arbitrary uses, is vital to ensure that everyone, regardless of socio-economic status, can participate equitably.
     </div>
 
     <!-- Second row: card then picture -->
     <div class="skeleton-card-second">
-        Blockchain technology, at its core, is about ensuring authenticity in a digital age. As we transition into an era where our identities, assets, and transactions are increasingly digital, the importance of a secure and transparent system becomes paramount. This is where blockchain shines, offering a decentralized and immutable ledger that can verify and record digital transactions without the need for intermediaries. But beyond the technicalities, it's the potential societal impacts of blockchain that truly excite me. The promise of a more transparent, equitable, and decentralized world, where power and control are distributed among its participants, is a vision worth striving for. And as a developer, I have the privilege and responsibility to contribute to this vision, one line of code at a time.
+        My proficiency in Typescript and Javascript isn't just about coding. It's a testament to the profound ripple effects that individual decisions in tech can create. Investing my energy and expertise in blockchain now means I'm not only shaping its present but also influencing its future trajectory. The choices made today in this space could have far-reaching impacts, potentially shaping facets of humanity in ways we might not yet fully grasp.
+
+Financial realms, from traditional stock markets to emerging crypto spaces, always attract a blend of visionaries and opportunists. While stock markets have evolved into esteemed institutions, the crypto world is on a similar journey of maturation. I'm eager to be part of this transformative path, contributing to its refinement and potential.
+
+ who made a small decision one day to name a function if.  And that if changed everything.  And thats what led me here, one question about what I stand for as a person, if, if this is what I believe in, then what are the consequences  if I don’t do it. I hope to work with only teams that feel the same passion.  Finances attract scammers, certainly.  But the stock financial sector doesnt let it define it  Rather, they are prestigious.  And one day, as the younger generations grow into the role, so will the new technology.  
+
     </div>
     <div class="narrative-second"></div>
 </div>
