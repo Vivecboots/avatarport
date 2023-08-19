bf23c44088c133da3260f856938111ae81912757
Vivecboots <Blairwin05@gmail.com>
2023-08-18 20:09:33 -0700

    added vh to step 8

--- src/lib/Step8.svelte
+++ src/lib/Step8.svelte
@@ -1,12 +1,20 @@
-<img src="/blockchainanimation.gif" alt="Blockchain" class="blockchain-img" />
+<div class="img-container">
+    <img src="/blockchainanimation.gif" alt="Blockchain" class="blockchain-img" />
+</div>
 
 <style>
-    .blockchain-img {
-        width: 30%;
-        height: 1%;
-        margin-right: 30%;
-        margin-left: 35%;
-    }
-</style>
+.img-container {
+    height: 55.66vh;
+    position: relative;
+    display: flex;
+    align-items: center; /* This will vertically center the image */
+    justify-content: center; /* This will horizontally center the image */
+    overflow: hidden; /* This ensures that the image doesn't spill out of the container */
+}
 
+.blockchain-img {
+    width: 30%;
+    max-height: 100%; /* This ensures the image doesn't exceed the container's height */
+}
+</style>
 
